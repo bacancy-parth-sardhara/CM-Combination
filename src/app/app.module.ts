13cd130c9c6244d64957a73b53331225a2f6223a
parthsardhara <parth.sardhara@bacancytechnology.com>
2019-02-03 15:46:13 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VehicalModule } from './vehical/vehical.module';
+import { ParentModule } from './parent/parent.module';
+import { UpperComponent } from './upper/upper.component';
+import { LowerComponent } from './upper/lower/lower.component';
+import { FirstComponent } from './first/first.component';
+import { SecondModule } from './first/second/second.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpperComponent,
+    LowerComponent,
+    FirstComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    VehicalModule,
+    ParentModule,
+    SecondModule
   ],
   providers: [],
   bootstrap: [AppComponent]
